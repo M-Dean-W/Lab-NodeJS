85ecb9b8e6082316d4eeb6d5ff69587be8425528
MD <mdeanwill@gmail.com>
2024-01-29 16:43:07 -0800

    part 1 complete

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const path = require('path')
+
+let chirps = ['tweet', 'yo', 'greetings', 'hi', 'cheep']
+
+const chirpsFile = path.join(__dirname, 'chirps.json' )
+
+fs.writeFile(chirpsFile, JSON.stringify(chirps), (er) => {
+    if (er) {
+        console.log(er)
+    }
+})
+
+fs.readFile(chirpsFile, (er,data) =>{
+    if(er) {
+        console.log(er)
+    }
+    console.log(JSON.parse(data))
+})
