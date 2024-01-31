ba7bb82308fbb54f206c446d217f585c745d36f1
MD <mdeanwill@gmail.com>
2024-01-31 15:02:58 -0800

    finished lab!

--- server/poster-downloader.js
+++ server/poster-downloader.js
@@ -1,15 +1,23 @@
 const axios = require('axios')
-
 const fs = require('fs')
 const path = require('path')
-const filmFile = path.join(__dirname, `downloads/random`)
 
-axios.get('https://api-ghibli.herokuapp.com/films')
-    .then((res) => res.json)
-    .then((data) => {
-        fs.writeFile(filmFile, JSON.stringify(data), (er) => {
-            if (er) {
-                console.log(er)
-            }
+
+
+fetch('https://api-ghibli.herokuapp.com/films')
+    .then((res) => res.json())
+    .then((films) => {
+        films.forEach(film => {
+            const image_url = film.movie_banner
+            const downDir = path.join(__dirname, `downloads`, `${film.title}.jpg`)
+            axios.get(image_url, { responseType: 'arraybuffer' })
+                .then(res => {
+                    fs.writeFile(downDir, res.data, (err) => {
+                        if (err) {
+                            console.log(err)
+                        }
+                    })
+                });
         })
     })
+
